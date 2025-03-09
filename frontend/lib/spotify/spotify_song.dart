130e5cfc0eb30217846fe647034fe6a36252981f
gallottino <d.gallotti@outlook.it>
2025-03-09 18:32:05 +0100

    feat: add integration with spotify api backend

---
+++ frontend/lib/spotify/spotify_song.dart
@@ -0,0 +1,36 @@
+class SpotifySong {
+  final String name;
+  final String artist;
+  final String album;
+  final String image;
+  final String uri;
+  String get id => uri.split(':')[2];
+
+  SpotifySong({
+    required this.name,
+    required this.artist,
+    required this.album,
+    required this.image,
+    required this.uri,
+  });
+
+  factory SpotifySong.fromJson(Map<String, dynamic> json) {
+    return SpotifySong(
+      name: json['name'],
+      artist: json['artist'],
+      album: json['album'],
+      image: json['image'],
+      uri: json['uri'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'artist': artist,
+      'album': album,
+      'image': image,
+      'uri': uri,
+    };
+  }
+}
